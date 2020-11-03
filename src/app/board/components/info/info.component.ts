b620c172520ce2104dc0795a6aaf72bd7e0dc54c
Thomas <thomas.gourouza@gmail.com>
2020-11-03 11:59:02 +0100

    améliorations composant info

--- src/app/board/components/info/info.component.ts
+++ src/app/board/components/info/info.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { normalize } from 'path';
 import { NewGame, Score } from '../../board.component';
 import { Square } from '../../services/board.service';
-import { Color } from '../../services/figure.service';
+import { Color, FigureName } from '../../services/figure.service';
 import { Move } from '../../services/history.service';
 
 @Component({
@@ -65,10 +66,61 @@ export class InfoComponent implements OnInit {
   }
 
   /**
-   * Traduction pour affichage de la couleur du trait
+   * Affiche le message de fin de partie ou informe du trait
    */
-  public traitInFrench(): string {
-    return this.trait === Color.black ? 'noirs' : 'blancs';
+  public infoText(): string {
+    return this.isEndGame()
+      ? this.endGameText
+      : 'Trait aux ' + (this.trait === Color.black ? 'noirs' : 'blancs');
+  }
+
+  /**
+   * Affiche les infos sur la pièce selectionnée (html)
+   */
+  public infoFigure(): string {
+    let infoFigure =
+      'Case ' +
+      this.selectedSquare.position.column.name +
+      this.selectedSquare.position.row.value +
+      ': ' +
+      this.figureToFrench(
+        this.selectedSquare.figure.name,
+        this.selectedSquare.figure.color
+      );
+    if (this.selectedSquare.figure.name != FigureName.king) {
+      infoFigure += ' (' + this.selectedSquare.figure.value + ')';
+    }
+    return infoFigure;
+  }
+
+  /**
+   * Affiche le nom de la pièce en français
+   *
+   * @param figureName
+   * @param color
+   */
+  private figureToFrench(figureName: FigureName, color: Color): string {
+    const isWhite = color === Color.white;
+    switch (figureName) {
+      case FigureName.pawn: {
+        return isWhite ? 'Pion blanc' : 'Pion noir';
+      }
+      case FigureName.bishop: {
+        return isWhite ? 'Fou blanc' : 'Fou noir';
+      }
+      case FigureName.rook: {
+        return isWhite ? 'Tour blanche' : 'Tour noire';
+      }
+      case FigureName.knight: {
+        return isWhite ? 'Cavalier blanc' : 'Cavalier noir';
+      }
+      case FigureName.queen: {
+        return isWhite ? 'Dame blanche' : 'Dame noire';
+      }
+      case FigureName.king: {
+        return isWhite ? 'Roi blanc' : 'Roi noir';
+      }
+    }
   }
 
   /**
